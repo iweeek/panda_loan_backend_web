ff9eb1625d6934faf7f9c715b018fbbf1a4d7e70
SmartNi <nijun717@gmail.com>
2017-12-07 15:09:27 +0800

    初步完成登录页

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import babelpolyfill from 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+import router from './router'
+import resources from './resources'
+import axios from 'axios'
+import $ from 'jquery'
+import md5 from 'js-md5'
+
+import 'font-awesome/css/font-awesome.min.css'
+
+Vue.use(ElementUI)
+
+Vue.prototype.$ajax = axios;
+
+new Vue({
+	router,
+	render: h => h(App)
+}).$mount('#app')
+
---
+++ src/resources.js
@@ -0,0 +1,24 @@
+const debug = process.env.NODE_ENV === 'development' ? true : false;
+// 测试环境地址
+if (debug) {
+    // var host = 'https://api.guangyangyundong.com/api';
+    // var host = 'http://120.77.72.16:8080/api';
+    //   var host = 'http://192.168.1.107:8080/api';
+    var host = 'http://localhost:8080';
+} else {
+    //正式环境地址
+    var host = 'https://api.guangyangyundong.com/api';
+}
+console.warn('当前resource: ', host);
+
+let resources = {
+    universityId: 1,
+    host: host,
+    // graphQL查询Api
+    graphQlApi: `${host}\/graphql`,
+    users(id) {
+        return `${host}\/users\/${id}`
+    }
+};
+
+export default resources;
