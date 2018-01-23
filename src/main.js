977410a3d9f3dfb2c076952468c213b5954bbae6
huwenqi <171484967@qq.com>
2018-01-23 14:16:10 +0800

    取消element-ui的引用

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import babelpolyfill from 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
-import ElementUI from 'element-ui'
+//import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 import router from './router'
 import resources from './resources'
@@ -41,7 +41,7 @@ import 'font-awesome/css/font-awesome.min.css'
 
 	Vue.component(Picker.name, Picker);
 
-	Vue.use(ElementUI)
+	//Vue.use(ElementUI)
 	Vue.prototype.$ajax = axios;
 	Vue.prototype.$toast = Toast
 
