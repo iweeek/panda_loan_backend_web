3e258bcd45a75dfe7175a3be142f88af2c001eaf
huwenqi <171484967@qq.com>
2018-02-07 15:49:06 +0800

    see #1341_1

--- src/router.js
+++ src/router.js
@@ -19,15 +19,156 @@ const Feedback = resolve => require.ensure([], () => resolve(require('./views/Fe
 const BindingBankcard = resolve => require.ensure([], () => resolve(require('./views/BindingBankcard.vue')), 'BindingBankcard')
 const AboutUs = resolve => require.ensure([], () => resolve(require('./views/AboutUs.vue')), 'AboutUs')
 
-//import test from './views/test.vue'
+const MSSDHelpCenter = resolve => require.ensure([], () => resolve(require('./views/mashangsudai/HelpCenter.vue')), 'MSSDHelpCenter')
+const MSSDCoupon = resolve => require.ensure([], () => resolve(require('./views/mashangsudai/Coupon.vue')), 'MSSDCoupon')
+const MSSDCouponRule = resolve => require.ensure([], () => resolve(require('./views/mashangsudai/CouponRule.vue')), 'MSSDCouponRule')
+const MSSDFeedback = resolve => require.ensure([], () => resolve(require('./views/mashangsudai/Feedback.vue')), 'MSSDFeedback')
+const MSSDBindingBankcard = resolve => require.ensure([], () => resolve(require('./views/mashangsudai/BindingBankcard.vue')), 'MSSDBindingBankcard')
+const MSSDAboutUs = resolve => require.ensure([], () => resolve(require('./views/mashangsudai/AboutUs.vue')), 'MSSDAboutUs')
+const MSSDFeeDeductionAgreement = resolve => require.ensure([], () => resolve(require('./views/mashangsudai/FeeDeductionAgreement.vue')), 'MSSDFeeDeductionAgreement')
+
+const PAKDHelpCenter = resolve => require.ensure([], () => resolve(require('./views/pingankuaidai/HelpCenter.vue')), 'PAKDHelpCenter')
+const PAKDCoupon = resolve => require.ensure([], () => resolve(require('./views/pingankuaidai/Coupon.vue')), 'PAKDCoupon')
+const PAKDCouponRule = resolve => require.ensure([], () => resolve(require('./views/pingankuaidai/CouponRule.vue')), 'PAKDCouponRule')
+const PAKDFeedback = resolve => require.ensure([], () => resolve(require('./views/pingankuaidai/Feedback.vue')), 'PAKDFeedback')
+const PAKDBindingBankcard = resolve => require.ensure([], () => resolve(require('./views/pingankuaidai/BindingBankcard.vue')), 'PAKDBindingBankcard')
+const PAKDAboutUs = resolve => require.ensure([], () => resolve(require('./views/pingankuaidai/AboutUs.vue')), 'PAKDAboutUs')
+const PAKDFeeDeductionAgreement = resolve => require.ensure([], () => resolve(require('./views/pingankuaidai/FeeDeductionAgreement.vue')), 'PAKDFeeDeductionAgreement')
+
+const YRJKHelpCenter = resolve => require.ensure([], () => resolve(require('./views/yirenjiekuan/HelpCenter.vue')), 'YRJKHelpCenter')
+const YRJKCoupon = resolve => require.ensure([], () => resolve(require('./views/yirenjiekuan/Coupon.vue')), 'YRJKCoupon')
+const YRJKCouponRule = resolve => require.ensure([], () => resolve(require('./views/yirenjiekuan/CouponRule.vue')), 'YRJKCouponRule')
+const YRJKFeedback = resolve => require.ensure([], () => resolve(require('./views/yirenjiekuan/Feedback.vue')), 'YRJKFeedback')
+const YRJKBindingBankcard = resolve => require.ensure([], () => resolve(require('./views/yirenjiekuan/BindingBankcard.vue')), 'YRJKBindingBankcard')
+const YRJKAboutUs = resolve => require.ensure([], () => resolve(require('./views/yirenjiekuan/AboutUs.vue')), 'YRJKAboutUs')
 
 let routes = [
-    // {
-    //     path: '/test',
-    //     component: test
-    // },
     {
-        path: '/feedback',
+        path: '/MSSDfeedback',
+        component: MSSDFeedback,
+        name: '意见反馈',
+        hidden: true
+    },
+    {
+        path: '/MSSDbindingBankcard',
+        component: MSSDBindingBankcard,
+        name: '绑定银行卡',
+        hidden: true
+    },
+    {
+        path: '/MSSDcouponRule',
+        component: MSSDCouponRule,
+        name: '优惠券规则',
+        hidden: true
+    },
+    {
+        path: '/MSSDaboutUs',
+        component: MSSDAboutUs,
+        name: '关于我们',
+        hidden: true
+    },
+    {
+        path:'/MSSDcoupon',
+        component: MSSDCoupon,
+        name: '我的优惠券',
+        hidden: true
+    },
+    {
+        path:'/MSSDhelpCenter',
+        component: MSSDHelpCenter,
+        name: '帮助中心',
+        hidden: true
+    },
+    {
+        path:'/mssdfeeDeductionAgreement',
+        component: MSSDFeeDeductionAgreement,
+        name: '自动划扣协议',
+        hidden: true
+    },
+
+    {
+        path: '/pakdfeedback',
+        component: PAKDFeedback,
+        name: '意见反馈',
+        hidden: true
+    },
+    {
+        path: '/pakdbindingBankcard',
+        component: PAKDBindingBankcard,
+        name: '绑定银行卡',
+        hidden: true
+    },
+    {
+        path: '/pakdcouponRule',
+        component: PAKDCouponRule,
+        name: '优惠券规则',
+        hidden: true
+    },
+    {
+        path: '/pakdaboutUs',
+        component: PAKDAboutUs,
+        name: '关于我们',
+        hidden: true
+    },
+    {
+        path:'/pakdcoupon',
+        component: PAKDCoupon,
+        name: '我的优惠券',
+        hidden: true
+    },
+    {
+        path:'/pakdhelpCenter',
+        component: PAKDHelpCenter,
+        name: '帮助中心',
+        hidden: true
+    },
+    {
+        path:'/pakdfeeDeductionAgreement',
+        component: PAKDFeeDeductionAgreement,
+        name: '自动划扣协议',
+        hidden: true
+    },
+    
+    
+    {
+        path: '/yrjkfeedback',
+        component: YRJKFeedback,
+        name: '意见反馈',
+        hidden: true
+    },
+    {
+        path: '/yrjkbindingBankcard',
+        component: YRJKBindingBankcard,
+        name: '绑定银行卡',
+        hidden: true
+    },
+    {
+        path: '/yrjkcouponRule',
+        component: YRJKCouponRule,
+        name: '优惠券规则',
+        hidden: true
+    },
+    {
+        path: '/yrjkaboutUs',
+        component: YRJKAboutUs,
+        name: '关于我们',
+        hidden: true
+    },
+    {
+        path:'/yrjkcoupon',
+        component: YRJKCoupon,
+        name: '我的优惠券',
+        hidden: true
+    },
+    {
+        path:'/yrjkhelpCenter',
+        component: YRJKHelpCenter,
+        name: '帮助中心',
+        hidden: true
+    },
+
+    {
+        path: '/feedBack',
         component: Feedback,
         name: '意见反馈',
         hidden: true
@@ -41,7 +182,7 @@ let routes = [
     {
         path: '/myCoupon',
         component: MyCoupon,
-        name: '我的优惠券',
+        name: '优惠券规则',
         hidden: true
     },
     {
