bad554ab5021f6ca24755c9861ae9dd0d0b17c5d
DESKTOP-NMOGKK9\dedao <363358873@qq.com>
2018-03-19 17:08:09 +0800

    see #1400_1

---
+++ src/router/mobileycling.js
@@ -0,0 +1,351 @@
+ // ios黄色手机回收H5页面
+ const XHBKAboutUs = resolve => require.ensure([], () => resolve(require('../views/mobilePhonerecycling/AboutUs.vue')), 'XHBKAboutUs')
+ const XHBKAddress = resolve => require.ensure([], () => resolve(require('../views/mobilePhonerecycling/Address.vue')), 'XHBKAddress') //地址
+ const XHBKSetaddress = resolve => require.ensure([], () => resolve(require('../views/mobilePhonerecycling/Setaddress.vue')), 'XHBKSetaddress') //新增地址
+ const XHBKHelpCenter = resolve => require.ensure([], () => resolve(require('../views/mobilePhonerecycling/HelpCenter.vue')), 'XHBKHelpCenter')
+ const XHBKMyCoupon = resolve => require.ensure([], () => resolve(require('../views/mobilePhonerecycling/MyCoupon.vue')), 'XHBKMyCoupon')
+ const XHBKBindingBankcard = resolve => require.ensure([], () => resolve(require('../views/mobilePhonerecycling/BindingBankcard.vue')), 'XHBKBindingBankcard')
+ const XHBKCoupon = resolve => require.ensure([], () => resolve(require('../views/mobilePhonerecycling/Coupon.vue')), 'XHBKCoupon') //增价卷
+ const XHBKmobilePhonerecyclingFeedback = resolve => require.ensure([], () => resolve(require('../views/mobilePhonerecycling/Feedback.vue')), 'XHBKmobilePhonerecyclingFeedback')
+ const XHBKFeeDeductionAgreement = resolve => require.ensure([], () => resolve(require('../views/mobilePhonerecycling/FeeDeductionAgreement.vue')), 'XHBKFeeDeductionAgreement')
+
+ // 黄色安卓版本手机回收H5页面
+ const AndroidAboutUs = resolve => require.ensure([], () => resolve(require('../views/androidrecycling/AboutUs.vue')), 'AndroidAboutUs')
+ const AndroidAddress = resolve => require.ensure([], () => resolve(require('../views/androidrecycling/Address.vue')), 'AndroidAddress') //地址
+ const AndroidSetaddress = resolve => require.ensure([], () => resolve(require('../views/androidrecycling/Setaddress.vue')), 'AndroidSetaddress') //新增地址
+ const AndroidHelpCenter = resolve => require.ensure([], () => resolve(require('../views/androidrecycling/HelpCenter.vue')), 'AndroidHelpCenter')
+ const AndroidMyCoupon = resolve => require.ensure([], () => resolve(require('../views/androidrecycling/MyCoupon.vue')), 'AndroidMyCoupon')
+ const AndroidBindingBankcard = resolve => require.ensure([], () => resolve(require('../views/androidrecycling/BindingBankcard.vue')), 'AndroidBindingBankcard')
+ const AndroidCoupon = resolve => require.ensure([], () => resolve(require('../views/androidrecycling/Coupon.vue')), 'AndroidCoupon') //增价卷
+ const AndroidFeedback = resolve => require.ensure([], () => resolve(require('../views/androidrecycling/Feedback.vue')), 'AndroidFeedback')
+ const AndroidAgreement = resolve => require.ensure([], () => resolve(require('../views/androidrecycling/FeeDeductionAgreement.vue')), 'AndroidAgreement')
+
+ // ios蓝色手机回收H5页面
+ const IosBlueAboutUs = resolve => require.ensure([], () => resolve(require('../views/iosBluerecycling/AboutUs.vue')), 'IosBlueAboutUs')
+ const IosBlueAddress = resolve => require.ensure([], () => resolve(require('../views/iosBluerecycling/Address.vue')), 'IosBlueAddress') //地址
+ const IosBlueSetaddress = resolve => require.ensure([], () => resolve(require('../views/iosBluerecycling/Setaddress.vue')), 'IosBlueSetaddress') //新增地址
+ const IosBlueHelpCenter = resolve => require.ensure([], () => resolve(require('../views/iosBluerecycling/HelpCenter.vue')), 'IosBlueHelpCenter')
+ const IosBlueMyCoupon = resolve => require.ensure([], () => resolve(require('../views/iosBluerecycling/MyCoupon.vue')), 'IosBlueMyCoupon')
+ const IosBlueBindingBankcard = resolve => require.ensure([], () => resolve(require('../views/iosBluerecycling/BindingBankcard.vue')), 'IosBlueBindingBankcard')
+ const IosBlueCoupon = resolve => require.ensure([], () => resolve(require('../views/iosBluerecycling/Coupon.vue')), 'IosBlueCoupon') //增价卷
+ const IosBlueFeedback = resolve => require.ensure([], () => resolve(require('../views/iosBluerecycling/Feedback.vue')), 'IosBlueFeedback')
+ const IosBlueAgreement = resolve => require.ensure([], () => resolve(require('../views/iosBluerecycling/FeeDeductionAgreement.vue')), 'IosBlueAgreement')
+
+ //马上速贷手机回收H5页面
+ const MSSDHelpCenter = resolve => require.ensure([], () => resolve(require('../views/mashangsudai/HelpCenter.vue')), 'MSSDHelpCenter')
+ const MSSDCoupon = resolve => require.ensure([], () => resolve(require('../views/mashangsudai/Coupon.vue')), 'MSSDCoupon')
+ const MSSDCouponRule = resolve => require.ensure([], () => resolve(require('../views/mashangsudai/CouponRule.vue')), 'MSSDCouponRule')
+ const MSSDFeedback = resolve => require.ensure([], () => resolve(require('../views/mashangsudai/Feedback.vue')), 'MSSDFeedback')
+ const MSSDBindingBankcard = resolve => require.ensure([], () => resolve(require('../views/mashangsudai/BindingBankcard.vue')), 'MSSDBindingBankcard')
+ const MSSDAboutUs = resolve => require.ensure([], () => resolve(require('../views/mashangsudai/AboutUs.vue')), 'MSSDAboutUs')
+ const MSSDcoupon = resolve => require.ensure([], () => resolve(require('../views/mashangsudai/MyCoupon.vue')), 'MSSDcoupon')
+ const MSSDFeeDeductionAgreement = resolve => require.ensure([], () => resolve(require('../views/mashangsudai/FeeDeductionAgreement.vue')), 'MSSDFeeDeductionAgreement')
+
+ // 平安快贷手机回收H5页面
+
+ const PAKDHelpCenter = resolve => require.ensure([], () => resolve(require('../views/pingankuaidai/HelpCenter.vue')), 'PAKDHelpCenter')
+ const PAKDCoupon = resolve => require.ensure([], () => resolve(require('../views/pingankuaidai/Coupon.vue')), 'PAKDCoupon')
+ const PAKDCouponRule = resolve => require.ensure([], () => resolve(require('../views/pingankuaidai/CouponRule.vue')), 'PAKDCouponRule')
+ const PAKDFeedback = resolve => require.ensure([], () => resolve(require('../views/pingankuaidai/Feedback.vue')), 'PAKDFeedback')
+ const PAKDBindingBankcard = resolve => require.ensure([], () => resolve(require('../views/pingankuaidai/BindingBankcard.vue')), 'PAKDBindingBankcard')
+ const PAKDAboutUs = resolve => require.ensure([], () => resolve(require('../views/pingankuaidai/AboutUs.vue')), 'PAKDAboutUs')
+ const PAKDFeeDeductionAgreement = resolve => require.ensure([], () => resolve(require('../views/pingankuaidai/FeeDeductionAgreement.vue')), 'PAKDFeeDeductionAgreement')
+
+ // 伊人借款h5页面
+ const YRJKHelpCenter = resolve => require.ensure([], () => resolve(require('../views/yirenjiekuan/HelpCenter.vue')), 'YRJKHelpCenter')
+ const YRJKCoupon = resolve => require.ensure([], () => resolve(require('../views/yirenjiekuan/Coupon.vue')), 'YRJKCoupon')
+ const YRJKCouponRule = resolve => require.ensure([], () => resolve(require('../views/yirenjiekuan/CouponRule.vue')), 'YRJKCouponRule')
+ const YRJKFeedback = resolve => require.ensure([], () => resolve(require('../views/yirenjiekuan/Feedback.vue')), 'YRJKFeedback')
+ const YRJKBindingBankcard = resolve => require.ensure([], () => resolve(require('../views/yirenjiekuan/BindingBankcard.vue')), 'YRJKBindingBankcard')
+ const YRJKAboutUs = resolve => require.ensure([], () => resolve(require('../views/yirenjiekuan/AboutUs.vue')), 'YRJKAboutUs')
+ const YRJKFeeDeductionAgreement = resolve => require.ensure([], () => resolve(require('../views/yirenjiekuan/FeeDeductionAgreement.vue')), 'YRJKFeeDeductionAgreement')
+
+ export default [
+
+     // 手机回收h5页面
+     {
+         path: '/XHBKHelpCenter',
+         component: XHBKHelpCenter,
+         name: '帮助中心',
+         hidden: true
+     },
+     {
+         path: '/XHBKAboutUs',
+         component: XHBKAboutUs,
+         name: '关于',
+         hidden: true
+     },
+     {
+         path: '/XHBKSetaddress',
+         component: XHBKSetaddress,
+         name: '添加地址',
+         hidden: true
+     },
+     {
+         path: '/XHBKAddress',
+         component: XHBKAddress,
+         name: '我的收货地址',
+         hidden: true
+     },
+     {
+         path: '/XHBKCoupon',
+         component: XHBKCoupon,
+         name: '我的加价卷',
+         hidden: true
+     },
+     {
+         path: '/XHBKmobilePhonerecyclingFeedback',
+         component: XHBKmobilePhonerecyclingFeedback,
+         name: '意见反馈',
+         hidden: true
+     },
+     {
+         path: '/XHBKbindingBankcard',
+         component: XHBKBindingBankcard,
+         name: '绑定银行卡',
+         hidden: true
+     },
+     {
+         path: '/XHBKfeeDeductionAgreement',
+         component: XHBKFeeDeductionAgreement,
+         name: '自动划扣协议',
+         hidden: true
+     },
+     {
+         path: '/XHBKMyCoupon',
+         component: XHBKMyCoupon,
+         name: '使用规则',
+         hidden: true
+     },
+
+     //  安卓部分（黄色）
+     {
+         path: '/androidAboutUs',
+         component: AndroidAboutUs,
+         name: '熊猫贷款'
+     },
+     { //添加地址
+         path: '/androidSetaddress',
+         component: AndroidSetaddress,
+         name: '熊猫贷款'
+     },
+     { //常见问题
+         path: '/androidHelpCenter',
+         component: AndroidHelpCenter,
+         name: '熊猫贷款'
+     },
+     { //使用规则
+         path: '/androidMyCoupon',
+         component: AndroidMyCoupon,
+         name: '熊猫贷款'
+     },
+     { //绑定银行卡
+         path: '/androidBindingBankcard',
+         component: AndroidBindingBankcard,
+         name: '熊猫贷款'
+     },
+     { //优惠券
+         path: '/androidCoupon',
+         component: AndroidCoupon,
+         name: '熊猫贷款'
+     },
+     { //反馈
+         path: '/androidFeedback',
+         component: AndroidFeedback,
+         name: '熊猫贷款'
+     },
+     { //协议
+         path: '/androidAgreement',
+         component: AndroidAgreement,
+         name: '熊猫贷款'
+     },
+     { //已有地址
+         path: '/androidAddress',
+         component: AndroidAddress,
+         name: '熊猫贷款'
+     },
+
+     // ios蓝色手机回收页面
+
+     { //关于我们
+         path: '/iosBlueAboutUs',
+         component: IosBlueAboutUs,
+         name: '熊猫贷款'
+     },
+     {
+         path: '/iosBlueAddress',
+         component: IosBlueAddress,
+         name: '熊猫贷款'
+     },
+     {
+         path: '/iosBlueSetaddress',
+         component: IosBlueSetaddress,
+         name: '熊猫贷款'
+     },
+     {
+         path: '/iosBlueHelpCenter',
+         component: IosBlueHelpCenter,
+         name: '熊猫贷款'
+     },
+     {
+         path: '/iosBlueMyCoupon',
+         component: IosBlueMyCoupon,
+         name: '熊猫贷款'
+     },
+     {
+         path: '/iosBlueBindingBankcard',
+         component: IosBlueBindingBankcard,
+         name: '熊猫贷款'
+     },
+     {
+         path: '/iosBlueCoupon',
+         component: IosBlueCoupon,
+         name: '熊猫贷款'
+     },
+     {
+         path: '/iosBlueFeedback',
+         component: IosBlueFeedback,
+         name: '熊猫贷款'
+     },
+     {
+         path: '/iosBlueAgreement',
+         component: IosBlueAgreement,
+         name: '熊猫贷款'
+     },
+
+
+     //  马上速贷手机回收
+     {
+         path: '/MSSDfeedback',
+         component: MSSDFeedback,
+         name: '意见反馈',
+         hidden: true
+     },
+     {
+         path: '/MSSDbindingBankcard',
+         component: MSSDBindingBankcard,
+         name: '绑定银行卡',
+         hidden: true
+     },
+     {
+         path: '/MSSDcouponRule',
+         component: MSSDCouponRule,
+         name: '优惠券规则',
+         hidden: true
+     },
+     {
+         path: '/MSSDaboutUs',
+         component: MSSDAboutUs,
+         name: '关于我们',
+         hidden: true
+     },
+     {
+         path: '/MSSDcoupon',
+         component: MSSDCoupon,
+         name: '我的优惠券',
+         hidden: true
+     },
+     {
+         path: '/MSSDhelpCenter',
+         component: MSSDHelpCenter,
+         name: '帮助中心',
+         hidden: true
+     },
+     {
+         path: '/mssdfeeDeductionAgreement',
+         component: MSSDFeeDeductionAgreement,
+         name: '自动划扣协议',
+         hidden: true
+     },
+
+
+
+     // 平安快贷H5回收
+     // {
+     //     path: '/pakdfeedback',
+     //     component: PAKDFeedback,
+     //     name: '意见反馈',
+     //     hidden: true
+     // },
+     // {
+     //     path: '/pakdbindingBankcard',
+     //     component: PAKDBindingBankcard,
+     //     name: '绑定银行卡',
+     //     hidden: true
+     // },
+     // {
+     //     path: '/pakdcouponRule',
+     //     component: PAKDCouponRule,
+     //     name: '优惠券规则',
+     //     hidden: true
+     // },
+     // {
+     //     path: '/pakdaboutUs',
+     //     component: PAKDAboutUs,
+     //     name: '关于我们',
+     //     hidden: true
+     // },
+     // {
+     //     path: '/pakdcoupon',
+     //     component: PAKDCoupon,
+     //     name: '我的优惠券',
+     //     hidden: true
+     // },
+     // {
+     //     path: '/pakdhelpCenter',
+     //     component: PAKDHelpCenter,
+     //     name: '帮助中心',
+     //     hidden: true
+     // },
+     // {
+     //     path: '/pakdfeeDeductionAgreement',
+     //     component: PAKDFeeDeductionAgreement,
+     //     name: '自动划扣协议',
+     //     hidden: true
+     // },
+
+
+     // 宜人借款h5页面
+     // {
+     //     path: '/yrjkfeedback',
+     //     component: YRJKFeedback,
+     //     name: '意见反馈',
+     //     hidden: true
+     // },
+     // {
+     //     path: '/yrjkbindingBankcard',
+     //     component: YRJKBindingBankcard,
+     //     name: '绑定银行卡',
+     //     hidden: true
+     // },
+     // {
+     //     path: '/yrjkcouponRule',
+     //     component: YRJKCouponRule,
+     //     name: '优惠券规则',
+     //     hidden: true
+     // },
+     // {
+     //     path: '/yrjkaboutUs',
+     //     component: YRJKAboutUs,
+     //     name: '关于我们',
+     //     hidden: true
+     // },
+     // {
+     //     path: '/yrjkcoupon',
+     //     component: YRJKCoupon,
+     //     name: '我的优惠券',
+     //     hidden: true
+     // },
+     // {
+     //     path: '/yrjkhelpCenter',
+     //     component: YRJKHelpCenter,
+     //     name: '帮助中心',
+     //     hidden: true
+     // },
+     // {
+     //     path: '/yrjkfeeDeductionAgreement',
+     //     component: YRJKFeeDeductionAgreement,
+     //     name: '自动划扣协议',
+     //     hidden: true
+     // },
+ ]
