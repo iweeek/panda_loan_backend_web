284b1974b863c5a5734d08133118af12e547df05
DESKTOP-NMOGKK9\dedao <363358873@qq.com>
2018-04-02 16:48:26 +0800

    see #1417_3

--- src/router/mycenter.js
+++ src/router/mycenter.js
@@ -1,10 +1,22 @@
-const MyConter = resolve => require.ensure([], () => resolve(require('../views/mycenter/myConter.vue')), 'MyConter') //个人中心页面测试
+const Xyconter = resolve => require.ensure([], () => resolve(require('../views/mycenter/Xyconter.vue')), 'Xyconter') //小赢白卡个人中心页面
+const Mcconter = resolve => require.ensure([], () => resolve(require('../views/mycenter/mcconter.vue')), 'Mcconter') //莫愁周转
+const Xhconter = resolve => require.ensure([], () => resolve(require('../views/mycenter/xhconter.vue')), 'Xhconter') //小花白卡
 
 export default [
 
     {
-        path: '/myConter',
-        component: MyConter,
+        path: '/xyconter',
+        component: Xyconter,
+        hidden: true
+    },
+    {
+        path: '/mcconter',
+        component: Mcconter,
+        hidden: true
+    },
+    {
+        path: '/xhconter',
+        component: Xhconter,
         hidden: true
     },
 ]
